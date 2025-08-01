0453ef2f0b62c723768910b2212d7dae4aa925bc
konstancio <konstancio@gmail.com>
2025-08-01 23:51:04 +0200

    Update calcular_intervalos.py

--- calcular_intervalos.py
+++ calcular_intervalos.py
@@ -1,8 +1,6 @@
-from astral.sun import sun
+from astral.sun import sun, elevation
 from astral.location import Observer
-from astral import sun as astral_sun
 from datetime import datetime, timedelta
-from astral import solar
 
 def calcular_intervalos_optimos(lat, lon, hoy=None, timezone_str='Europe/Madrid'):
     if hoy is None:
@@ -11,14 +9,14 @@ def calcular_intervalos_optimos(lat, lon, hoy=None, timezone_str='Europe/Madrid'
         hoy = hoy.date()
 
     observer = Observer(latitude=lat, longitude=lon)
-    s = astral_sun.sun(observer, date=hoy, tzinfo=timezone_str)
+    s = sun(observer, date=hoy, tzinfo=timezone_str)
 
     elevaciones = []
     hora_actual = s['sunrise']
     fin = s['sunset']
 
     while hora_actual <= fin:
-        altitud = solar.elevation(observer, hora_actual)
+        altitud = elevation(observer, hora_actual)
         if 30 <= altitud <= 40:
             elevaciones.append(hora_actual.strftime('%H:%M'))
         hora_actual += timedelta(minutes=10)
@@ -28,6 +26,7 @@ def calcular_intervalos_optimos(lat, lon, hoy=None, timezone_str='Europe/Madrid'
     despues = [h for h in elevaciones if datetime.strptime(h, '%H:%M').time() > mediodia.time()]
 
     return antes, despues
+
 
 
 
